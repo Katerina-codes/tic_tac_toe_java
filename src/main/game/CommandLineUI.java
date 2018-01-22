a3359d2ea9ba7ad61ede754300909c69e0d8e7d0
Katerina <katerinaldg@gmail.com>
2018-01-22 11:46:32 +0000

    Get player move
    - Add input stream to CommandLineUi.

--- src/main/game/CommandLineUI.java
+++ src/main/game/CommandLineUI.java
@@ -1,14 +1,16 @@
 package main.game;
 
-import java.io.PrintStream;
+import java.io.*;
 import java.util.List;
 
 public class CommandLineUI {
 
     private final PrintStream output;
+    private final BufferedReader input;
 
-    public CommandLineUI(PrintStream output) {
+    public CommandLineUI(PrintStream output, InputStream input) {
         this.output = output;
+        this.input = new BufferedReader(new InputStreamReader(input));
     }
     public void displayBoard(List<List<String>> rows) {
         StringBuilder sb = new StringBuilder();
@@ -32,5 +34,15 @@ public class CommandLineUI {
 
     public void askForMove() {
         output.println("Place your mark! Pick a move from 1 - 9:");
+    }
+
+    public String getPlayerMove() {
+        String move = null;
+        try {
+            return input.readLine();
+        } catch (IOException e) {
+            System.out.println("Nothing was entered.");
+        }
+        return move;
     }
 }
--- src/test/game/CommandLineUITest.java
+++ src/test/game/CommandLineUITest.java
@@ -3,11 +3,14 @@ package test.game;
 import main.game.CommandLineUI;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CommandLineUITest {
@@ -16,7 +19,8 @@ public class CommandLineUITest {
     public void boardIsDisplayed() {
         List<List<String>> rows = asList(asList("1", "2", "3"), asList("4", "5", "6"), asList("7", "8", "9"));
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        CommandLineUI inputOutput = new CommandLineUI(new PrintStream(output));
+        InputStream input = new ByteArrayInputStream("".getBytes());
+        CommandLineUI inputOutput = new CommandLineUI(new PrintStream(output), input);
 
         inputOutput.displayBoard(rows);
 
@@ -28,10 +32,20 @@ public class CommandLineUITest {
     @Test
     public void askPlayerForMove() {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        CommandLineUI inputOutput = new CommandLineUI(new PrintStream(output));
+        InputStream input = new ByteArrayInputStream("".getBytes());
+        CommandLineUI inputOutput = new CommandLineUI(new PrintStream(output), input);
 
         inputOutput.askForMove();
 
         assertTrue(output.toString().contains("Place your mark! Pick a move from 1 - 9:"));
+    }
+
+    @Test
+    public void getPlayerMove() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        InputStream input = new ByteArrayInputStream("1".getBytes());
+        CommandLineUI inputOutput = new CommandLineUI(new PrintStream(output), input);
+
+        assertEquals("1", inputOutput.getPlayerMove());
     }
 }
