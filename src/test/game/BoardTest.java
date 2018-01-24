d8b02beed6a15a9f9b92ae6851b83c30d5933898
Katerina <katerinaldg@gmail.com>
2018-01-24 09:56:46 +0000

    Correct tests
    - Use board instance variable to remove duplication.

--- src/test/game/BoardTest.java
+++ src/test/game/BoardTest.java
@@ -39,16 +39,16 @@ public class BoardTest {
 
     @Test
     public void playersCantEnterSameMove() {
-        Board board = new Board(asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
-        List<String> grid = asList("X", "2", "3", "4", "5", "6", "7", "8", "9");
+        Board board = new Board(asList("X", "2", "3", "4", "5", "6", "7", "8", "9"));
+        List<String> grid = board.board;
 
         assertFalse(board.isMoveUnique(grid, 1));
     }
 
     @Test
     public void boardHasNoAvailableMovesLeft() {
-        Board board = new Board(asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
-        List<String> grid = asList("X", "O", "X", "O", "X", "O", "X", "O", "X");
+        Board board = new Board(asList("X", "O", "X", "O", "X", "O", "X", "O", "X"));
+        List<String> grid = board.board;
 
         assertFalse(board.hasAvailableMoves(grid));
     }
