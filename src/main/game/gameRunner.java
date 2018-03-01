f56efb26104f2dd70aaf30f4c62b03053f120860
Katerina <katerinaldg@gmail.com>
2018-03-01 15:34:26 +0000

    Extract magic numbers to constants

--- src/main/game/gameRunner.java
+++ src/main/game/gameRunner.java
@@ -2,15 +2,19 @@ package main.game;
 
 public class gameRunner {
 
+    public static final int THREE_BY_THREE = 3;
+    public static final int FOUR_BY_FOUR = 4;
+    public static final int FIRST_CHOICE = 1;
+
     public static void main(String[] args) {
         CommandLineUI inputOutput = new CommandLineUI(System.out, System.in);
         inputOutput.askForBoardSize();
         int userChoice = Integer.parseInt(inputOutput.getUserChoice());
         int boardSize;
-        if (userChoice == 1) {
-            boardSize = 3;
+        if (userChoice == FIRST_CHOICE) {
+            boardSize = THREE_BY_THREE;
         } else {
-            boardSize = 4;
+            boardSize = FOUR_BY_FOUR;
         }
         Board board = new Board(boardSize);
         Game game = new Game(inputOutput, board);
