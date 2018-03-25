ed08bfb2f06f3eb49734b262108f7bae31c9470f
Katerina <katerinaldg@gmail.com>
2018-03-25 22:13:31 +0100

    Computer blocks move

--- src/test/game/UnbeatableComputerTest.java
+++ src/test/game/UnbeatableComputerTest.java
@@ -20,5 +20,15 @@ public class UnbeatableComputerTest {
                 EMPTY, EMPTY, O));
         assertThat(unbeatableComputer.findBestMove(board, true, -1), is(7));
     }
+
+    @Test
+    public void computerBlocksMove() {
+        UnbeatableComputer unbeatableComputer = new UnbeatableComputer(O);
+        Board board = new Board(3, asList(
+                X, X, EMPTY,
+                O, O, X,
+                EMPTY, X, O));
+        assertThat(unbeatableComputer.findBestMove(board, false, -1), is(2));
+    }
 }
 
