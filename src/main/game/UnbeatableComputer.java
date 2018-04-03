5c81f028dbf6b23b740ad5ae156d98a2ebd2dac9
katerina georgiou <katerinageorgiou@georginas-MBP.localdomain>
2018-04-03 16:10:37 +0100

    Minimax is working
    - Remove BestScore class and return array of best move and best value.
    - Remove maxMove and minMove methods as if statement filters best move and value.
    - Correct game mode display.
    - Delete test that is no longer needed.

--- src/main/game/UnbeatableComputer.java
+++ src/main/game/UnbeatableComputer.java
@@ -1,18 +1,15 @@
 package main.game;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class UnbeatableComputer implements Player {
     private Mark mark;
-    private int bestMove2;
 
     public UnbeatableComputer(Mark mark) {
         this.mark = mark;
     }
-
-    private List<BestScore> scores = new ArrayList<>();
 
     @Override
     public Mark getMark() {
@@ -21,22 +18,17 @@ public class UnbeatableComputer implements Player {
 
     @Override
     public Board playMove(Board board) {
-        findBestMove(board, true);
-        board.updateMove(bestMove2, mark);
-        return board;
+        int move = findBestMove(board,true).get(0);
+        return board.updateMove(move, mark);
     }
 
-    public int findBestMove(Board board, Boolean maximisingPlayer) {
+    public List<Integer> findBestMove(Board board, Boolean maximisingPlayer) {
         Mark opponentMark = mark == Mark.O ? Mark.X : Mark.O;
+        int bestMove = -50;
 
         if (board.gameIsOver()) {
-            if (board.playerHasWon(mark)) {
-                return 1;
-            } else if (board.playerHasWon(opponentMark)) {
-                return -1;
-            } else if (board.gameIsTied()) {
-                return 0;
-            }
+            int gameScore = gameScore(board, opponentMark);
+            return asList(1, gameScore);
         }
 
         if (maximisingPlayer) {
@@ -44,58 +36,41 @@ public class UnbeatableComputer implements Player {
             List<Integer> possibleMoves = board.availableMoves();
             for (int move : possibleMoves) {
                 board = board.updateMove(move, mark);
-                int currentScore = findBestMove(board, false);
-                bestValue = bestValue > currentScore ? bestValue : currentScore;
+                int currentScore = findBestMove(board, false).get(1);
+
+                if (currentScore > bestValue) {
+                    bestValue = currentScore;
+                    bestMove = move;
+                }
+
                 board = board.updateMove(move, Mark.EMPTY);
-                scores.add(new BestScore(move, bestValue));
             }
-            bestMove2 = maxMove(scores).getMove();
-            return maxMove(scores).getMoveScore();
+            return asList(bestMove, bestValue);
         } else {
             int bestValue = +10;
             List<Integer> possibleMoves = board.availableMoves();
             for (int move : possibleMoves) {
                 board = board.updateMove(move, opponentMark);
-                int currentScore = findBestMove(board, true);
-                bestValue = bestValue < currentScore ? bestValue : currentScore;
+                int currentScore = findBestMove(board, true).get(1);
+
+                if (currentScore < bestValue) {
+                    bestValue = currentScore;
+                    bestMove = move;
+                }
+
                 board = board.updateMove(move, Mark.EMPTY);
-                scores.add(new BestScore(move, bestValue));
             }
-            bestMove2 = minMove(scores).getMove();
-            return minMove(scores).getMoveScore();
+            return asList(bestMove, bestValue);
         }
     }
 
-    public BestScore maxMove(List<BestScore> scores) {
-        BestScore highestScoringMove = scores.stream()
-                .max(Comparator.comparing(BestScore::getMoveScore))
-                .orElse(new BestScore(-1, -1));
-        return highestScoringMove;
-    }
-
-    public BestScore minMove(List<BestScore> scores) {
-        BestScore lowestScoringMove = scores.stream()
-                .min(Comparator.comparing(BestScore::getMoveScore))
-                .orElse(new BestScore(-1, -1));
-        return lowestScoringMove;
-    }
-
-    public static class BestScore {
-
-        private final int move;
-        private final int moveScore;
-
-        public BestScore(int move, int moveScore) {
-            this.move = move;
-            this.moveScore = moveScore;
-        }
-
-        public int getMoveScore() {
-            return moveScore;
-        }
-
-        public int getMove() {
-            return move;
+    private int gameScore(Board board, Mark opponentMark) {
+        if (board.playerHasWon(mark)) {
+            return 1;
+        } else if (board.playerHasWon(opponentMark)) {
+            return -1;
+        } else {
+            return 0;
         }
     }
 }
